dc9be2061480324fd8880b136e961b3c04f2f1af
hassehulabeck <hans.2.andersson@educ.goteborg.se>
2024-02-02 22:00:45 +0100

    Added shifting colors to the dots

--- index.js
+++ index.js
@@ -21,14 +21,32 @@ buttons.addEventListener('click', (e) => {
 })
 
 const graphScore = () => {
+
+    const scoreColors = [
+        "#1177ff",
+        "#2288dd",
+        "#3399bb",
+        "#44aa99",
+        "#55bb88",
+        "#66cc66",
+        "#77dd44",
+        "#88ee33",
+        "#99ff22",
+        "#aaee11",
+        "#bbdd00"
+    ]
+
     playerScore.innerHTML = "<article class='tag'>Player</article>"
     cpuScore.innerHTML = "<article class='tag'>CPU</article>"
     for (let i=0; i < score.player; i++) {
         let playerdot = document.createElement("div")
+        playerdot.style = "background-color:" + scoreColors[i]
         playerScore.appendChild(playerdot)
     }
     for (let i=0; i < score.cpu; i++) {
         let cpudot = document.createElement("div")
+        cpudot.style = "background-color:" + scoreColors[10 - i]
         cpuScore.appendChild(cpudot)
     }
 }
+
