58568a531144ff2647a2e5df6057c441d2846506
hassehulabeck <hans.2.andersson@educ.goteborg.se>
2024-02-02 21:38:57 +0100

    Rudimentary graphical score

--- index.js
+++ index.js
@@ -4,16 +4,31 @@ let paper = document.getElementById("paper")
 let scissors = document.getElementById("scissors")
 let cpu = document.getElementById("cpu")
 let scoreInfo = document.getElementById("score")
+let playerScore = document.getElementById("playerScore")
+let cpuScore = document.getElementById("cpuScore")
 
 import {select, referee, score} from './sxp.js'
 
-scoreInfo.innerHTML = "<p>Player: " + score.player + " - " + score.cpu + " CPU</p>" 
+//scoreInfo.innerHTML = "<p>Player: " + score.player + " - " + score.cpu + " CPU</p>" 
 
 buttons.addEventListener('click', (e) => {
     let player = e.target.id
     let cpuWeapon = select()
     cpu.innerHTML = "<p>" + cpuWeapon + "</p>"
     referee(cpuWeapon, player)
-    scoreInfo.innerHTML = "<p>Player: " + score.player + " - " + score.cpu + " CPU</p>" 
     console.log(score)
+    graphScore()
 })
+
+const graphScore = () => {
+    playerScore.innerHTML = ""
+    cpuScore.innerHTML = ""
+    for (let i=0; i < score.player; i++) {
+        let playerdot = document.createElement("div")
+        playerScore.appendChild(playerdot)
+    }
+    for (let i=0; i < score.cpu; i++) {
+        let cpudot = document.createElement("div")
+        cpuScore.appendChild(cpudot)
+    }
+}
